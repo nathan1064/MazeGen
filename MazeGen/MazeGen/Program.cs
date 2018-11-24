4cf9295058050aa28fa82bcf5ccdab05a7a9fb5e
nathan1064 <nathan.b1064@gmail.com>
2018-11-24 14:44:32 +1300

    Implement the maze class, and adjacency list. initialize adjacency list to all true in the constructor

--- MazeGen/MazeGen/Program.cs
+++ MazeGen/MazeGen/Program.cs
@@ -19,4 +19,21 @@ namespace MazeGen
             Application.Run(new Form1());
         }
     }
+
+    public class Maze
+    {
+        public bool[,,] adjacencyList = new bool[20,20,2];
+
+        public Maze()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 20; j++)
+                {
+                    adjacencyList[i, j, 0] = true;
+                    adjacencyList[i, j, 1] = true;
+                }
+            }
+        }
+    }
 }
